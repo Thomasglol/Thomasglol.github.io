13a564b96e802f61672f1d0d4fe198b11ef45e92
Thomasglol <78051359+Thomasglol@users.noreply.github.com>
2021-01-26 13:40:09 -0800

    uploaded fact finder for the first time 1/26/21

---
+++ factfinder/script.js
@@ -0,0 +1,26 @@
+
+
+document.querySelector("#a").addEventListener("click", showa);
+function showa(){
+	document.getElementById("factresult").innerHTML = "<h1>The unicorn is the national animal of Scotland.</h1><br><img src='https://www.sciencenewsforstudents.org/wp-content/uploads/2020/03/1030_onwardunicorns-1.png' alt='picture of unicorn'></img>";
+}
+
+document.querySelector("#b").addEventListener("click", showb);
+function showb(){
+	document.getElementById("factresult").innerHTML = "<h1>The largest known living organism is an aspen grove.</h1><br><img src='https://preview.redd.it/p50pgs6vlqp31.jpg?width=960&crop=smart&auto=webp&s=b91a2b5c8039f70f47fe57834339484adada0720'></img>";
+}
+
+document.querySelector("#c").addEventListener("click", showc);
+function showc(){
+	document.getElementById("factresult").innerHTML = "<h1>The wood frog can hold its pee for up to eight months.</h1><br><img src='https://i0.wp.com/bestlifeonline.com/wp-content/uploads/2020/08/Wood-frog.jpg?resize=1024%2C683&ssl=1'></img>";
+}
+
+document.querySelector("#d").addEventListener("click", showd);
+function showd(){
+	document.getElementById("factresult").innerHTML = "<h1>Cotton candy was invented by a dentist.</h1><br><img src='https://i1.wp.com/bestlifeonline.com/wp-content/uploads/2020/08/Rainbow-cotton-candy.jpg?resize=1024%2C646&ssl=1'></img>";
+}
+
+document.querySelector("#e").addEventListener("click", showe);
+function showe(){
+	document.getElementById("factresult").innerHTML = "<h1>The English word with the most definitions is \"set\".</h1><br><img src='https://i2.wp.com/bestlifeonline.com/wp-content/uploads/2020/08/Shelve-of-dictionaries.jpg?resize=1024%2C620&ssl=1'></img>";
+}
